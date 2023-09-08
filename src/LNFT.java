ad1c93f04b06e4cac1cab6cb73b4446ffc25e2fb
David <hyt152004@gmail.com>
2023-09-07 20:42:20 -0700

    first commit

--- src/LNFT.java
+++ src/LNFT.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println("Hello, GitHub!");
+        System.out.println("Hello, GitHubWJTATT!");
     }
 }
