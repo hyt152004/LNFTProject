a58afd67e8ede0cbedfb53c4eb4450674134bb8b
David <hyt152004@gmail.com>
2023-09-07 21:29:52 -0700

    hello justin!

--- src/LNFT.java
+++ src/LNFT.java
@@ -1,6 +1,6 @@
 public class LNFT {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello World!");
-        System.out.println("Hello GitHub");
+        System.out.println("Hello GitHub hello justin");
     }
 }
